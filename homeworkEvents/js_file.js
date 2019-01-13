96ba8fa58bf1a8248621f97f0e712afcb4999af3
ArevikT <areviktovmasyan7@gmail.com>
2019-01-14 02:46:29 +0300

    did homevork

---
+++ homeworkEvents/js_file.js
@@ -0,0 +1,10 @@
+ function myFunction() {
+    var x = document.getElementById("circle_1").style;
+    // x.animationName = "circle1";
+    x.animation = "circle1 3s forwards";
+    var y = document.getElementById("circle_2").style;
+    // y.animationName = "circle2";
+    y.animation = "circle2 3s forwards";
+    
+//  }
+}
