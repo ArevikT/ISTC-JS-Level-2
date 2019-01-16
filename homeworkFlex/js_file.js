faab8b2ee4aab3f7a8d2cec1f8501935b74611f0
ArevikT <areviktovmasyan7@gmail.com>
2019-01-16 22:41:11 +0300

    changes

--- homeworkFlex/js_file.js
+++ homeworkFlex/js_file.js
@@ -1,9 +1,11 @@
+
+document.getElementsByTagName("ul").addEventListener("click", myFunction);
 function myFunction{
-    var x = document.getElementsByTagName("ul");
-    // if (x.style.display === "none"){
-    //     x.style.display = "blok";
-    // } else {
-    //     x.style.display = "none";
-    x.style.display = "blok";
+   // var x = document.getElementsByTagName("ul");
+    if (x.style.display === "none"){
+        x.style.display = "blok";
+    } else {
+        x.style.display = "none";
+
       }
 }
