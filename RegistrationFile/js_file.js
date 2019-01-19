1c78d8d69dac8d6099a0f7b078e9d6275735bcc8
ArevikT <areviktovmasyan7@gmail.com>
2019-01-19 15:08:19 +0400

    ...

--- RegistrationFile/js_file.js
+++ RegistrationFile/js_file.js
@@ -16,21 +16,33 @@ function myFunction() {
          };
         console.log(obj);
         var myJsonObj = JSON.stringify(obj);
+        console.log(myJsonObj);
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 var data = JSON.parse(this.responseText);
-                 console.log(data);
+
+                 console.log(data.id);
             }
           };
+
+            
            xhttp.open("POST", "http://rest.learncode.academy/api/learncode/friends", true);
-            xhttp.setRequestHeader("Content-type", "application/JSON");
+            xhttp.setRequestHeader("Content-type", "application/json");
             xhttp.send(myJsonObj);
-           console.log(myJsonObj);
-    
-           
+           console.log(myJsonObj, "myjsonobject");
+          
+          
+    //     fetch('http://rest.learncode.academy/api/learncode/friends', {
+    //      method: "POST",
+    //      headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(obj),
+    // })
+    // .then(response => response.json()) // response.json() returns a promise
+    // .then((response) => {
+    //   console.log("You saved this item", response); //returns the new item along with its ID
+    // })
 
-        
-  }
-  
-
+        }
